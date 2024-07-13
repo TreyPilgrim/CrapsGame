d8a08dfc9354e399a768b0d100c149f34a1c75cf
Trey Fullwood <treyfullwood3@gmail.com>
2024-07-13 14:51:32 -0500

    Implemented base class LinkedList and fixed Node bugs

--- LinkedList.h
+++ LinkedList.h
@@ -1,5 +1,23 @@
 #pragma once
 #include <iostream>
 #include <memory>
+#include "Node.h"
+#include <string.h>
+using namespace std;
+
+using ptr = shared_ptr<Node>;
+using betNode = shared_ptr<BetNode>;
+using playerNode = shared_ptr<PlayerNode>;
 
 class LinkedList;
+
+class LinkedList
+{
+private:
+    ptr head = make_shared<Node>();
+    ptr tail = make_shared<Node>();
+
+public:
+    LinkedList() = default;
+    ~LinkedList() {}
+};

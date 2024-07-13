3a793d993a79f690d72d767fd4a8d40f4e949cdc
Trey Fullwood <treyfullwood3@gmail.com>
2024-07-12 23:23:08 -0500

    Added Dice Functionality in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,58 @@
 #include <iostream>
+#include <memory>
+#include <cstdlib>
+#include <ctime>
 #include "BetsLinkedList.h"
+
+// Function Prototypes
+int randNum();
+int *rollDice();
+void Craps();
 
 int main()
 {
-    Bets tmp;
+    // Ensure all numbers generated randomly are actually random
+    srand(time(NULL));
 
-    tmp.push();
-    tmp.push();
-    tmp.display();
+    char quit = 'N';
+
+    while (quit != 'y' && quit != 'Y')
+    {
+        Craps();
+        std::cout << "quit game? " << std::endl;
+        std::cin >> quit;
+    }
     return 0;
+}
+
+// Function for rolling the Dice
+// Generate random #
+int randNum()
+{
+    // Generate 3 random #'s to ensure radomness
+    int dice;
+    for (int i = 0; i < 3; i++)
+        dice = (rand() % 6) + 1;
+
+    // Return dice value
+    return dice;
+}
+int *rollDice()
+{
+
+    static int theDie[2];
+    // Assign two dice values
+    theDie[0] = randNum();
+    theDie[1] = randNum();
+
+    // return pointer
+    return theDie;
+}
+void Craps()
+{
+    int *theDie;
+    theDie = rollDice();
+
+    std::cout << "Dice 1 value: " << theDie[0] << std::endl;
+    std::cout << "Dice 2 value: " << theDie[1] << std::endl;
 }
